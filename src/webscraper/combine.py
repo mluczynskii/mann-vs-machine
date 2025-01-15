7e86754916357a4f71dc3af1bbe800c73df08ca9
Mateusz Łuczyński <mateusz.luczynski02@gmail.com>
2025-01-15 20:38:37 +0100

    Add notebook for generating AI-tweets with gpt-neo

--- src/webscraper/combine.py
+++ src/webscraper/combine.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import os 
+import csv
+
+AI_FILE = "ai-tweets.csv"
+HUMAN_FOLDER = "data"
 
 dataset = set()
 
 for filename in os.listdir("data/"):
-  with open(f'data/{filename}', "r") as input:
-    for tweet in input:
-      dataset.add(tweet)
+  with open(f"{HUMAN_FOLDER}/{filename}", "r") as input_human:
+    for tweet in input_human:
+      dataset.add((tweet, 0))
 
-with open("tweets.csv", "w") as output:
-  for tweet in dataset:
-    output.write(tweet)
+with open(AI_FILE, "r") as input_ai:
+  for tweet in input_ai:
+    dataset.add((tweet, 1))
+
+with open("dataset.csv", "w", newline='') as output:
+  writer = csv.DictWriter(output, fieldnames=['txt', 'lbl'], quotechar="'")
+  for (tweet, label) in dataset:
+    writer.writerow({"txt": ' '.join(tweet.split()), "lbl": label})
